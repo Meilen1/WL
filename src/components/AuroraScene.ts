f0f6cf60d6dc118a3331f298481b205e9036b24a
Rocio Belen Casco <rbcasco@ausa.com.ar>
2026-01-16 11:36:04 -0300

    deploy fix

--- src/components/AuroraScene.ts
+++ src/components/AuroraScene.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { auroraVertex, auroraFragment } from "./auroraShaders.ts";
+import { auroraVertex, auroraFragment } from "./auroraShaders";
 
 export function initAurora(container: HTMLDivElement) {
     console.log("INIT AURORA");
--- src/scripts/aurora.client.ts
+++ src/scripts/aurora.client.ts
@@ -1,9 +1,17 @@
 import { initAurora } from "../components/AuroraScene";
 
-const el = document.getElementById("aurora") as HTMLDivElement | null;
+function start() {
+  const el = document.getElementById("aurora") as HTMLDivElement | null;
+  console.log("SCRIPT CLIENTE", el);
 
-console.log("SCRIPT CLIENTE", el);
+  if (el) {
+    initAurora(el);
+  }
+}
 
-if (el) {
-  initAurora(el);
+// 🔥 CLAVE: esperar DOM
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", start);
+} else {
+  start();
 }
