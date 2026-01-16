227e092865511b7e185b00b9f5b3b30a0db1c237
Rocio Belen Casco <rbcasco@ausa.com.ar>
2026-01-16 14:42:47 -0300

    deploy fix 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  site: "https://meilen1.github.io",
-  base: "/WL/",
-  output: "static",
+  adapter: vercel(),
 });
