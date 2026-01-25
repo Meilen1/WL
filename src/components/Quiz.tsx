471118e2786e12fe2ac31a850ffbf22110d57d7c
Meilen <rocio.casco@davinci.edu.ar>
2026-01-25 02:09:50 -0300

    nuevo endpoint

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import type { Question } from "../types/Question";
 
-const API_URL = "https://competitions-noon-disciplines-initiative.trycloudflare.com";
+const API_URL = "https://complex-warnings-synthetic-destination.trycloudflare.com";
 
 export default function Quiz() {
   const [questions, setQuestions] = useState<Question[]>([]);
